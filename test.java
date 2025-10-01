3446f6ef1854018ea78a7d5ec807aa634d61d26b
Nobberu <itsnobberu@gmail.com>
2025-10-01 13:52:55 +0700

    test

--- test.java
+++ test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("halo hai teman");
+        System.out.println("pergantian tes merge conflict.");
     }
 }
